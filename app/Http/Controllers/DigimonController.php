cbc16dd3bb698b12838086a55fbb5fa2af8a35a2
Domeénica Jimeénez <domenica.jimenez@epn.edu.ec>
2023-12-21 13:03:55 -0500

    Fix DigimonController functions delete jwt

--- app/Http/Controllers/DigimonController.php
+++ app/Http/Controllers/DigimonController.php
@@ -9,32 +9,28 @@ class DigimonController extends Controller
 {
     /**
      * Display a listing of digimons per page.
-     * TODO: validate jwt
      */
     public function digimonList(Request $request)
     {
-        $bodyContent = json_decode($request->getContent());
-        if(!isset($bodyContent->pageSize) || !isset($bodyContent->page) || !isset($bodyContent->token)) {
+        if(!isset($request->pageSize) || !isset($request->page)) {
             return ["error"=>400,"message"=>"Bad request: Some parameters are missing"];
         }
         $digimons = Http::get('https://digi-api.com/api/v1/digimon', [
-            'pageSize' => $bodyContent->pageSize,
-            'page' => $bodyContent->page,
+            'pageSize' => $request->pageSize,
+            'page' => $request->page,
         ]);
         return json_decode($digimons->body());
     }
 
     /**
      * Display the specified digimon.
-     * TODO: validate jwt
      */
     public function showDigimon(Request $request)
     {
-        $bodyContent = json_decode($request->getContent());
-        if(!isset($bodyContent->id) || !isset($bodyContent->token)) {
+        if(!isset($request->id)) {
             return ["error"=>400,"message"=>"Bad request: Some parameters are missing"];
         }
-        $digimon = Http::get('https://digi-api.com/api/v1/digimon/'.strval($bodyContent->id));
+        $digimon = Http::get('https://digi-api.com/api/v1/digimon/'.strval($request->id));
         return json_decode($digimon->body());
     }
 
